05da45239f0fa8930e2e1d7c3084a4f0f6a01773
unknown <daniel.cortes.83944@gmail.com>
2021-09-03 19:50:22 -0500

    feat: Fixed indentation

--- routes/auth.js
+++ routes/auth.js
@@ -14,60 +14,62 @@ const User = require('../models/User')
 // @desc Get logged in user
 // @access Private
 router.get('/', auth, async (req, res) => {
-  try {
-    const user = await User.findById(req.user.id).select('-password')
-    res.json(user)
-  } catch (err) {
-    console.error(err.message)
-    res.status(500).send('Server error')
-  }
+  	try {
+    	const user = await User.findById(req.user.id).select('-password')
+    	res.json(user)
+  	} catch (err) {
+    	console.error(err.message)
+    	res.status(500).send('Server error')
+  	}
 })
 
 // @route POST api/auth
 // @desc Auth user and get token
 // @access Public
-router.post('/', [
-  check('email', 'Please include a valid email').isEmail(),
-  check('password', 'Password is reuqired').exists()
-], async (req, res) => {
+router.post('/', 
+	[
+  		check('email', 'Please include a valid email').isEmail(),
+  		check('password', 'Password is reuqired').exists()
+	], 
+	async (req, res) => {
 
-  const errors = validationResult(req)
+  		const errors = validationResult(req)
 
-    if(!errors.isEmpty()){
-      return res.status(400).json({ errors: errors.array() })
-    } 
+    	if(!errors.isEmpty()){
+      		return res.status(400).json({ errors: errors.array() })
+    	} 
   
-    const { email, password } = req.body
+    	const { email, password } = req.body
 
-    try {
-      let user = await User.findOne({ email})
+    	try {
+    		let user = await User.findOne({ email})
 
-      if(!user){
-        return res.status(400).json({ msg: 'Invalid credentials'})
-      }
+      		if(!user){
+        		return res.status(400).json({ msg: 'Invalid credentials'})
+      		}
 
-      const isMatch = await bcrypt.compare(password, user.password)
+	      	const isMatch = await bcrypt.compare(password, user.password)
 
-      if(!isMatch){
-        return res.status(400).json({ msg: 'Invalid Credentials'})
-      }
+      		if(!isMatch){
+        		return res.status(400).json({ msg: 'Invalid Credentials'})
+      		}
 
-      const payload = {
-        user: {
-          id: user.id
-        }
-      }
+      		const payload = {
+        		user: {
+          			id: user.id
+        		}
+      		}
 
-      jwt.sign(payload, config.get('jwtsecret'), {
-        expiresIn: 3600000
-      }, (err, token) => {
-        if(err) throw err;
-        res.json({ token })
-      })
-    } catch (err) {
-      console.error(err.message)
-      res.status(500).send('Server Error')
-    }
+      		jwt.sign(payload, config.get('jwtsecret'), {
+        		expiresIn: 3600000
+      		}, (err, token) => {
+        		if(err) throw err;
+        		res.json({ token })
+      		})
+    	} catch (err) {
+      		console.error(err.message)
+      		res.status(500).send('Server Error')
+    	}
 })
 
 module.exports = router
--- routes/users.js
+++ routes/users.js
@@ -11,62 +11,60 @@ const User = require('../models/User')
 // @desc Register a user
 // @access Public
 router.post(
-  '/', 
-  [
-    check('name', 'name is required')
-      .not()
-      .isEmpty(),
-    check('email', 'please include a valid email')
-      .isEmail(),
-    check('password', 'Please enter a assword with six or more characters')
-      .isLength({ min: 6})
+  	'/', 
+  	[
+    	check('name', 'name is required')
+      		.not()
+      		.isEmpty(),
+    	check('email', 'please include a valid email')
+      		.isEmail(),
+    	check('password', 'Please enter a assword with six or more characters')
+      		.isLength({ min: 6})
   ], 
   async (req, res) => {
-    const errors = validationResult(req)
+	const errors = validationResult(req)
 
     if(!errors.isEmpty()){
-      return res.status(400).json({ errors: errors.array() })
+    	return res.status(400).json({ errors: errors.array() })
     }
 
     const { email, name, password } = req.body
 
     try {
-      let user = await User.findOne({ email })
+    	let user = await User.findOne({ email })
 
-      if(user){
-        return res.status(400).json({msg: 'User already exists'})
-      }
+    	if(user){
+        	return res.status(400).json({msg: 'User already exists'})
+      	}
 
-      user = new User({
-        name, 
-        email, 
-        password
-      })
+    	user = new User({
+        	name, 
+        	email, 
+        	password
+      	})
 
-      const salt = await bcrypt.genSalt(10)
+    	const salt = await bcrypt.genSalt(10)
 
-      user.password = await bcrypt.hash(password, salt)
+    	user.password = await bcrypt.hash(password, salt)
 
-      await user.save()
+    	await user.save()
 
-      const payload = {
-        user: {
-          id: user.id
-        }
-      }
+    	const payload = {
+        	user: {
+          		id: user.id
+        	}
+      	}
 
-      jwt.sign(payload, config.get('jwtsecret'), {
-        expiresIn: 3600000
-      }, (err, token) => {
-        if(err) throw err;
-        res.json({ token })
-      })
-
-      
+    	jwt.sign(payload, config.get('jwtsecret'), {
+        	expiresIn: 3600000
+      	}, (err, token) => {
+        	if(err) throw err;
+        	res.json({ token })
+      	})
 
     } catch (err) {
-      console.error(err.message)
-      res.status(500).send('Server Error')
+    	console.error(err.message)
+    	res.status(500).send('Server Error')
     }
   }
 )
